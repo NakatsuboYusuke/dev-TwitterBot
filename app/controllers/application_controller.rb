737880aafe5ef9540694e6bb9ac26193653709af
NakatsuboYusuke <>
2020-01-19 23:17:26 +0900

    Twitterに投稿するためのMVCを作成しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  require 'twitter'
+  protect_from_forgery with: :exception
+  before_action :set_client
+
+  private
+
+  def set_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.credentials.twitter[:api_key]
+      config.consumer_secret = Rails.application.credentials.twitter[:api_secret_key]
+      config.access_token = Rails.application.credentials.twitter[:access_token]
+      config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
+    end
+  end
 end
